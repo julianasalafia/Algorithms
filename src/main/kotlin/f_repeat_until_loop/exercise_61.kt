d887a99665df0d271d18deae23508a5c473167eb
julianasalafia <juliana.salafia1@gmail.com>
2023-04-22 18:17:35 -0300

    repeat.. until loop

---
+++ src/main/kotlin/f_repeat_until_loop/exercise_61.kt
@@ -0,0 +1,6 @@
+package f_repeat_until_loop
+
+/*
+    61) Create a program that shows the following count on the screen using the “repeat until” structure:
+    0 3 6 9 12 15 18 21 24 27 30 Done!
+*/
---
+++ src/main/kotlin/f_repeat_until_loop/exercise_63.kt
@@ -0,0 +1,10 @@
+package f_repeat_until_loop
+
+/*
+63) Create a program using the “repeat until” structure that reads several numbers. At each loop, ask the user
+whether they want to continue or not. At the end, show on the screen:
+a) The sum of all values
+b) What was the smallest value entered
+c) The average of all values
+d) How many values are even
+*/
