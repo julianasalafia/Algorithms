db9d8441f76d2e00c69f1eeca50c4f4e052eb4ea
julianasalafia <juliana.salafia1@gmail.com>
2023-06-19 15:10:54 -0300

    [BCS]: exercise 3

--- Dart/a_basic_control_structure/exercise_3.dart
+++ Dart/a_basic_control_structure/exercise_3.dart
@@ -1,1 +1,26 @@
-// Create a program that reads an employee's name and salary, and displays a message at the end.
+// Create a program that reads an employee's name and salary and displays a message at the end.
+
+import 'dart:io';
+
+void main() {
+  double? salary;
+
+  stdout.write("what's your name? ");
+  String name = stdin.readLineSync()!;
+
+  stdout.write("what's your salary? ");
+  while (salary == null) {
+    String? input = stdin.readLineSync();
+
+    if (input != null) {
+      try {
+        salary = double.parse(input);
+      } catch (exception) {
+        print("please, enter a valid number.");
+      }
+    }
+  }
+  String _salary = salary.toStringAsFixed(2);
+
+  print("the employee $name has a salary of US$_salary.");
+}
