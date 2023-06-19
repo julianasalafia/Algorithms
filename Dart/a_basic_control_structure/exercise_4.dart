38286f2af89f1a490331572510aba1042c1aa8dc
julianasalafia <juliana.salafia1@gmail.com>
2023-06-19 15:27:21 -0300

    [BCS]: exercise 4

--- Dart/a_basic_control_structure/exercise_4.dart
+++ Dart/a_basic_control_structure/exercise_4.dart
@@ -1,1 +1,18 @@
 // Develop an algorithm that reads two integers and displays the sum between them.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('1st value: ');
+  int firstValue = int.parse(stdin.readLineSync()!);
+
+  stdout.write('2nd value: ');
+  int secondValue = int.parse(stdin.readLineSync()!);
+
+  sum(firstValue, secondValue);
+}
+
+void sum(int firstValue, int secondValue) {
+  int result = firstValue + secondValue;
+  print('The sum between $firstValue and $secondValue is $result.');
+}
