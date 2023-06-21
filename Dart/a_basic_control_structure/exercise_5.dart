8e83e6aae056002805a08aaef664616d9d943e79
julianasalafia <juliana.salafia1@gmail.com>
2023-06-21 13:18:24 -0300

    [BCS]: exercise 5

--- Dart/a_basic_control_structure/exercise_5.dart
+++ Dart/a_basic_control_structure/exercise_5.dart
@@ -1,1 +1,18 @@
 // Write a program that reads a student's two grades in a subject and displays their average on the screen.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('1st grade: ');
+  double firstValue = double.parse(stdin.readLineSync()!);
+
+  stdout.write('2nd grade: ');
+  double secondValue = double.parse(stdin.readLineSync()!);
+
+  averageGrade(firstValue, secondValue);
+}
+
+void averageGrade(double value1, double value2) {
+  double average = (value1 + value2) / 2;
+  print('The average grade between $value1 and $value2 is $average.');
+}
