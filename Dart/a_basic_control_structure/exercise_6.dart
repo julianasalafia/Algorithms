f8bc996e44df747ab8dff949f94523ef60cdb5f5
julianasalafia <juliana.salafia1@gmail.com>
2023-06-22 10:24:30 -0300

    [BCS]: exercise 6

--- Dart/a_basic_control_structure/exercise_6.dart
+++ Dart/a_basic_control_structure/exercise_6.dart
@@ -1,1 +1,33 @@
 // Write a program that reads an integer and displays its predecessor and successor.
+
+import 'dart:io';
+
+void main() {
+  tryAgain();
+}
+
+void predecessorAndSucessor(int number) {
+  int predecessor = number - 1;
+  int successor = number + 1;
+
+  print(
+    'the predecessor of $number is $predecessor; the successor of $number is $successor.',
+  );
+}
+
+void tryAgain() {
+  String answer;
+
+  do {
+    stdout.write('write a number: ');
+    try {
+      int number = int.parse(stdin.readLineSync()!);
+      predecessorAndSucessor(number);
+    } catch (exception) {
+      print('Error: Please enter a valid integer');
+    }
+
+    stdout.write('do you want to try again? ');
+    answer = stdin.readLineSync()!;
+  } while (answer.toLowerCase() == 'yes');
+}
