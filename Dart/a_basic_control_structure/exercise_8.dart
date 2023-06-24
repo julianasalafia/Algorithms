2c52aab9e19668951e1bb122a0b3c67bb79199c5
julianasalafia <juliana.salafia1@gmail.com>
2023-06-24 10:03:51 -0300

    [BCS]: exercise 8

--- Dart/a_basic_control_structure/exercise_8.dart
+++ Dart/a_basic_control_structure/exercise_8.dart
@@ -1,1 +1,26 @@
 // Create an algorithm that reads a distance in meters and shows the corresponding values in other measures.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Distance in meters: ');
+  double distance = double.parse(stdin.readLineSync()!);
+
+  Map<String, double> units = {
+    'km': 0.001,
+    'hm': 0.01,
+    'dam': 0.1,
+    'dm': 10,
+    'cm': 100,
+    'mm': 1000
+  };
+
+  print('The distance of $distance corresponds to: ');
+  units.forEach((unit, factor) {
+    print('${convert(distance, factor)}$unit');
+  });
+}
+
+double convert(double distance, double factor) {
+  return distance * factor;
+}
