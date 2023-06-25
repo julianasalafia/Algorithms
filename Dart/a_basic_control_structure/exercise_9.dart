f0fb16eca9cd6565d1deb0ebea0f905abdb28db4
julianasalafia <juliana.salafia1@gmail.com>
2023-06-25 09:34:03 -0300

    [BCS]: exercise 9

--- Dart/a_basic_control_structure/exercise_9.dart
+++ Dart/a_basic_control_structure/exercise_9.dart
@@ -1,1 +1,35 @@
 // Write an algorithm that reads how much money a person has in their wallet (in R$) and shows how many dollars they can buy. Consider US$1.00 = R$3.45.
+
+import 'dart:io';
+
+void main() {
+  double exchangeRate = 3.45;
+  double amount = getAmount();
+
+  buyDollar(amount, exchangeRate);
+}
+
+double getAmount() {
+  while (true) {
+    stdout.write('Amount: R\$');
+    String? input = stdin.readLineSync();
+
+    if (input != null) {
+      try {
+        return double.parse(input);
+      } catch (exception) {
+        print('Error: Please enter a valid integer');
+      }
+    }
+  }
+}
+
+void buyDollar(double amount, double exchangeRate) {
+  String _amount = amount.toStringAsFixed(2);
+
+  double dollar = amount / exchangeRate;
+  String _dollar = dollar.round().toStringAsFixed(2);
+
+  print(
+      'You have R\$$_amount, the exchange rate is R$exchangeRate and you can buy US\$${_dollar}.');
+}
