1b0a2ce928be2c6169486f98d315fd77829eea22
julianasalafia <juliana.salafia1@gmail.com>
2023-07-05 15:04:41 -0300

    [BCS]: exercise 13; works but it is incomplete

--- Dart/a_basic_control_structure/exercise_13.dart
+++ Dart/a_basic_control_structure/exercise_13.dart
@@ -1,1 +1,20 @@
 // Create an algorithm that reads an employee's salary, calculates and shows their new salary with a 15% increase.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('name: ');
+  String name = stdin.readLineSync()!;
+
+  stdout.write('salary: ');
+  double salary = double.parse(stdin.readLineSync()!);
+
+  stdout.write('raise (%): ');
+  double percentage = double.parse(stdin.readLineSync()!);
+
+  double percentageConversion = percentage / 100;
+  double raiseAmount = salary * percentageConversion;
+  double newSalary = salary + raiseAmount;
+
+  print(newSalary);
+}
