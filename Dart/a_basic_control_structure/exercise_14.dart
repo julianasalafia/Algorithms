be8a1c6bdc5efed7e80188d02d981a95dec1b410
julianasalafia <juliana.salafia1@gmail.com>
2023-07-07 15:19:59 -0300

    [BCS]: exercise 14

--- Dart/a_basic_control_structure/exercise_14.dart
+++ Dart/a_basic_control_structure/exercise_14.dart
@@ -3,3 +3,21 @@
     kilometers traveled by a rented car and the number of days it was rented for.
     Calculate the total price to be paid, knowing that the car costs R$90 per day and R$0.20 per kilometer driven.
  */
+
+import 'dart:io';
+
+void main() {
+  stdout.write('km travelled: ');
+  double kmTravelled = double.parse(stdin.readLineSync()!);
+
+  stdout.write('days rented: ');
+  int daysRented = int.parse(stdin.readLineSync()!);
+
+  double km = kmTravelled * 0.20;
+  int days = daysRented * 90;
+
+  double totalPrice = km + days;
+
+  print(
+      'km travelled: $kmTravelled, days rented: $daysRented, total price: ${totalPrice.toStringAsFixed(2)}');
+}
