deb622e097d996e9a2d1d69506b6fb2c26ca8cd1
julianasalafia <juliana.salafia1@gmail.com>
2023-07-08 12:56:07 -0300

    [BCS]: exercise 16; works but incomplete

---
+++ Dart/a_basic_control_structure/exercise_16.dart
@@ -0,0 +1,24 @@
+/*
+    [CHALLENGE] Write a program to calculate the reduction in a smoker's lifetime.
+    Ask for the number of cigarettes smoked per day and how many years they have smoked.
+    Consider that a smoker loses 10 minutes of life for each cigarette.
+    Calculate how many days of life a smoker will lose and display the total in days.
+ */
+
+import 'dart:io';
+
+const double timeLostByCigar = 10 / 60.0;
+
+void main() {
+  stdout.write('Cigarettes smoked per day: ');
+  double smokedPerDay = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Years smoking: ');
+  double yearsSmoking = double.parse(stdin.readLineSync()!);
+
+  double totalTimeLost = smokedPerDay * timeLostByCigar * 365 * yearsSmoking;
+  int daysLost = totalTimeLost ~/ 24;
+
+  print(
+      'Cigarettes smoked per day: $smokedPerDay; years smoking: $yearsSmoking; days lost: $daysLost');
+}
