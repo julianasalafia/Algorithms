44b880a28006d64886d6989f577933965819df47
julianasalafia <juliana.salafia1@gmail.com>
2023-07-10 10:43:14 -0300

    [BC]: exercise 17

--- Dart/b_basic_conditions/exercise_17.dart
+++ Dart/b_basic_conditions/exercise_17.dart
@@ -2,3 +2,18 @@
      Write a program that asks for a car's speed. If it exceeds 80Km/h, display a message saying that the user has been fined.
      In this case, display the fine amount, charging R$5 for each Km above the speed limit.
 */
+
+import 'dart:io';
+
+void main() {
+  stdout.write('speed limit (km/h): ');
+  int speedLimit = int.parse(stdin.readLineSync()!);
+
+  stdout.write('car speed (km/h): ');
+  int carSpeed = int.parse(stdin.readLineSync()!);
+
+  if (carSpeed > speedLimit) {
+    double fineAmount = (carSpeed - speedLimit) * 5;
+    print('you\'ve been fined in US\$${fineAmount.toStringAsFixed(2)}');
+  }
+}
