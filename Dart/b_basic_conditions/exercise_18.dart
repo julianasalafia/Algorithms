7faaaff76b54006874112f6dfb57786b62b9093b
julianasalafia <juliana.salafia1@gmail.com>
2023-07-11 11:14:23 -0300

    [BC]: exercise 18

--- Dart/b_basic_conditions/exercise_18.dart
+++ Dart/b_basic_conditions/exercise_18.dart
@@ -1,2 +1,18 @@
 // Create a program that reads a person's birth year, calculates their age, and then shows whether they can vote.
 
+import 'dart:io';
+
+const currentYear = 2023;
+
+void main() {
+  stdout.write('birth year: ');
+  int birthYear = int.parse(stdin.readLineSync()!);
+
+  int age = currentYear - birthYear;
+
+  if (age >= 18) {
+    print('You can vote!');
+  } else {
+    print('You can\'t vote!');
+  }
+}
