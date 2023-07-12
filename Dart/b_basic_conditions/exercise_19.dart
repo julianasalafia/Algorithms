343488c027204e7d58e252fee55b65827e931a26
julianasalafia <juliana.salafia1@gmail.com>
2023-07-12 14:13:33 -0300

    [BC]: exercise 19

--- Dart/b_basic_conditions/exercise_19.dart
+++ Dart/b_basic_conditions/exercise_19.dart
@@ -6,24 +6,57 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('name: ');
-  String name = stdin.readLineSync()!;
+  String answer = 'yes';
 
-  stdout.write('G1: ');
-  double grade1 = double.parse(stdin.readLineSync()!);
+  while (answer == 'yes') {
+    String name = readString('name: ');
+    double grade1 = readDouble('G1: ');
+    double grade2 = readDouble('G2: ');
 
-  stdout.write('G2: ');
-  double grade2 = double.parse(stdin.readLineSync()!);
+    gpaCalculate(grade1, grade2, name);
 
-  gpaCalculate(grade1, grade2, name);
+    answer = readString('do you want to try again? ').toLowerCase();
+  }
 }
 
 void gpaCalculate(double grade1, double grade2, String name) {
   double gpa = (grade1 + grade2) / 2;
 
   if (gpa >= 7.0) {
-    print('The student $name did well. Average: $gpa');
+    print('The student $name did well. Average: $gpa.');
   } else {
-    print('The student $name did not do well. Average: $gpa');
+    print('The student $name did not do well. Average: $gpa.');
   }
+}
+
+double readDouble(String prompt) {
+  double? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = double.tryParse(input!);
+
+    if (value == null || value < 0 || value > 10) {
+      print('Error: Please enter a valid number');
+    }
+  } while (value == null || value < 0 || value > 10);
+
+  return value;
+}
+
+String readString(String prompt) {
+  String? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = input!;
+
+    if (value == null || !value.contains(RegExp(r'^[a-zA-Z\s]+$'))) {
+      print('Error: Please enter a valid string');
+    }
+  } while (value == null || !value.contains(RegExp(r'^[a-zA-Z\s]+$')));
+
+  return value;
 }
