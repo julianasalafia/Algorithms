8a465b16efdef879d75ee9e603fabc8e860d935e
julianasalafia <juliana.salafia1@gmail.com>
2023-07-13 11:12:44 -0300

    [BC]: exercise 20

--- Dart/b_basic_conditions/exercise_20.dart
+++ Dart/b_basic_conditions/exercise_20.dart
@@ -1,2 +1,17 @@
 // Develop a program that reads an integer and shows whether it is EVEN or ODD.
 
+import 'dart:io';
+
+void main() {
+  print('ODD OR EVEN');
+  print('----------------------------');
+
+  stdout.write('Number: ');
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number % 2 == 0) {
+    print('it\'s even!');
+  } else {
+    print('it\'s odd!');
+  }
+}
