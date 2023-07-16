11dabc7e2a6ea2593db3f470e40385ce0e900a79
julianasalafia <juliana.salafia1@gmail.com>
2023-07-16 11:42:24 -0300

    [BC]: exercise 23

--- Dart/b_basic_conditions/exercise_23.dart
+++ Dart/b_basic_conditions/exercise_23.dart
@@ -4,3 +4,34 @@
      Men receive a 5% discount
      Women receive a 13% discount
  */
+
+import 'dart:io';
+
+const int discountPercentageMale = 5;
+const int discountPercentageFemale = 13;
+
+void main() {
+  stdout.write('what is your name? ');
+  String name = stdin.readLineSync()!;
+
+  stdout.write('purchase price: ');
+  double purchasePrice = double.parse(stdin.readLineSync()!);
+
+  print('gender:\n1) male\n2) female\n3) other');
+
+  stdout.write(': ');
+  int option = int.parse(stdin.readLineSync()!);
+
+  double discountValueMale = (discountPercentageMale * purchasePrice) / 100;
+  double discountValueFemale = (discountPercentageFemale * purchasePrice) / 100;
+
+  if (option == 1) {
+    print('discount for $name: \$$discountValueMale');
+  } else if (option == 2) {
+    print('discount for $name: \$$discountValueFemale');
+  } else if (option == 3) {
+    print('User without discount, sign up.');
+  } else {
+    print('Invalid option.');
+  }
+}
