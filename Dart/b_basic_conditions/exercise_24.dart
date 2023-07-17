cf15799c76e24f84fbfa22bb562218680cba57f0
julianasalafia <juliana.salafia1@gmail.com>
2023-07-17 11:29:13 -0300

    [BC]: exercise 24

--- Dart/b_basic_conditions/exercise_24.dart
+++ Dart/b_basic_conditions/exercise_24.dart
@@ -5,17 +5,34 @@
 
 import 'dart:io';
 
+const priceUnder200km = 0.50;
+const priceAbove200km = 0.45;
+
 void main() {
-  stdout.write('Planned distance: ');
-  double plannedDistance = double.parse(stdin.readLineSync()!);
+  double plannedDistance = readDouble('Planned distance: ');
 
   if (plannedDistance > 0 && plannedDistance <= 200) {
-    double price = plannedDistance * 0.50;
+    double price = plannedDistance * priceUnder200km;
 
-    print('Distance: $plannedDistance. Price: $price');
+    print('Distance: $plannedDistance. Price: ${price.toStringAsFixed(2)}');
   } else {
-    double price = plannedDistance * 0.45;
+    double price = plannedDistance * priceAbove200km;
 
-    print('Distance: $plannedDistance. Price: $price');
+    print('Distance: $plannedDistance. Price: ${price..toStringAsFixed(2)}');
   }
+}
+
+double readDouble(String prompt) {
+  double? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = double.tryParse(input!);
+
+    if (value == null || value < 0) {
+      print('Error: Please enter a valid distance');
+    }
+  } while (value == null || value < 0);
+  return value;
 }
