270ee1847cb3d457eec7675e2f5881694bcb4551
julianasalafia <juliana.salafia1@gmail.com>
2023-07-18 10:00:01 -0300

    [BC]: exercise 25

--- Dart/b_basic_conditions/exercise_25.dart
+++ Dart/b_basic_conditions/exercise_25.dart
@@ -3,3 +3,20 @@
     if it is possible to form a triangle with these lines. Mathematically, for three segments to form a triangle,
     the length of each side must be less than the sum of the other two.
  */
+
+import 'dart:io';
+
+void main() {
+  stdout.write('A: ');
+  int a = int.parse(stdin.readLineSync()!);
+  stdout.write('B: ');
+  int b = int.parse(stdin.readLineSync()!);
+  stdout.write('C: ');
+  int c = int.parse(stdin.readLineSync()!);
+
+  if ((a < (b + c)) && (b < (a + c)) && (c < (a + b))) {
+    print('Triangle.');
+  } else {
+    print('Not a triangle.');
+  }
+}
