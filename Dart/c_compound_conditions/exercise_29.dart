b5f3ffe749641ab6beda765b28af42ae2bb46870
julianasalafia <juliana.salafia1@gmail.com>
2023-07-20 17:36:34 -0300

    [CC]: exercise 29

--- Dart/c_compound_conditions/exercise_29.dart
+++ Dart/c_compound_conditions/exercise_29.dart
@@ -2,20 +2,14 @@
 // at the company, and show his/her new salary, adjusted according to the following table:
 // Up to 3 years at the company: 3% increase. Between 3 and 10 years: 12.5% increase. 10 years or more: 20% increase.
 
-import 'dart:developer';
 import 'dart:io';
 
 enum ServiceTimeStatus { newbie, senior, veteran, error }
 
 void main() {
-  stdout.write('name: ');
-  String name = stdin.readLineSync()!;
-
-  stdout.write('salary: ');
-  double salary = double.parse(stdin.readLineSync()!);
-
-  stdout.write('service time: ');
-  double serviceTime = double.parse(stdin.readLineSync()!);
+  String name = readString('name: ').capitalize();
+  double salary = readDouble('salary: ');
+  double serviceTime = readDouble('service time: ');
 
   ServiceTimeStatus serviceTimeStatus = getServiceTimeStatus(serviceTime);
   printNewSalary(serviceTimeStatus, salary, name);
@@ -24,26 +18,34 @@ void main() {
 void printNewSalary(
     ServiceTimeStatus serviceTimeStatus, double salary, String name) {
   double? newSalary;
+  double? percentage;
+
+  double newbiePercentage = 3.5;
+  double seniorPercentage = 12.5;
+  double veteranPercentage = 20;
 
   switch (serviceTimeStatus) {
     case ServiceTimeStatus.newbie:
-      double newbieSalary = calculateRaise(salary, 3.5);
+      double newbieSalary = calculateRaise(salary, newbiePercentage);
       newSalary = newbieSalary;
+      percentage = newbiePercentage;
       break;
     case ServiceTimeStatus.senior:
       double seniorSalary = calculateRaise(salary, 12.5);
       newSalary = seniorSalary;
+      percentage = seniorPercentage;
       break;
     case ServiceTimeStatus.veteran:
-      double seniorSalary = calculateRaise(salary, 20.0);
-      newSalary = seniorSalary;
+      double veteranSalary = calculateRaise(salary, 20.0);
+      newSalary = veteranSalary;
+      percentage = veteranPercentage;
       break;
     case ServiceTimeStatus.error:
       print('ERROR.');
       break;
   }
   print(
-      'Name: $name \nPercentage:  \nSalary: $salary \nNew salary: $newSalary');
+      'Name: $name — Percentage: $percentage% — Salary: US\$${salary.toStringAsFixed(2)} — New salary: US\$${newSalary?.toStringAsFixed(2)}');
 }
 
 ServiceTimeStatus getServiceTimeStatus(double serviceTime) {
@@ -64,4 +66,42 @@ ServiceTimeStatus getServiceTimeStatus(double serviceTime) {
 double calculateRaise(double salary, double percentage) {
   double newSalary = salary + ((percentage * salary) / 100);
   return newSalary;
+}
+
+double readDouble(String prompt) {
+  double? value;
+
+  while (true) {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = double.tryParse(input!);
+
+    if (value != null && value >= 0) {
+      return value;
+    } else {
+      print('Error: Please enter a valid salary.');
+    }
+  }
+}
+
+String readString(String prompt) {
+  String? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = input!;
+
+    if (value == null || !value.contains(RegExp(r'^[a-zA-Z\s]+$'))) {
+      print('Error: Please enter a valid name');
+    }
+  } while (value == null || !value.contains(RegExp(r'^[a-zA-Z\s]+$')));
+
+  return value;
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+  }
 }
