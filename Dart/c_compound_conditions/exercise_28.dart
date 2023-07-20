d40f7f46e534028d9dc7bedfe84317420a24a8e3
julianasalafia <juliana.salafia1@gmail.com>
2023-07-19 23:48:16 -0300

    [CC]: exercise 28

--- Dart/c_compound_conditions/exercise_28.dart
+++ Dart/c_compound_conditions/exercise_28.dart
@@ -1,3 +1,25 @@
 // Make a program that reads the width and length of a rectangular terrain, calculating and showing its area in m².
 // The program should also show the classification of this land, according to the list below:
 // Below 100m² = POPULAR LAND. Between 100m² and 500m² = MASTER LAND. Above 500m² = VIP LAND.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('width: ');
+  double width = double.parse(stdin.readLineSync()!);
+
+  stdout.write('length: ');
+  double length = double.parse(stdin.readLineSync()!);
+
+  double area = width * length;
+
+  if (area >= 0 && area <= 99) {
+    print('Area: $area. \nPOPULAR LAND.');
+  } else if (area >= 100 && area <= 500) {
+    print('Area: $area. \nMASTER LAND.');
+  } else if (area > 500) {
+    print('Area: $area. \nVIP LAND.');
+  } else {
+    print('Invalid.');
+  }
+}
