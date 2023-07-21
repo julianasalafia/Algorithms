fa78e31474aacb08757a1ca54f9c913fa977b6f4
julianasalafia <juliana.salafia1@gmail.com>
2023-07-21 13:49:38 -0300

    [CC]: exercise 30

--- Dart/c_compound_conditions/exercise_30.dart
+++ Dart/c_compound_conditions/exercise_30.dart
@@ -1,2 +1,27 @@
 // [CHALLENGE] Redo algorithm 25, adding the feature of showing what type of triangle will be formed:
 // EQUILATERAL: all sides equal. ISOSCELES: two sides equal. SCALENE: all sides different.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('A: ');
+  int a = int.parse(stdin.readLineSync()!);
+
+  stdout.write('B: ');
+  int b = int.parse(stdin.readLineSync()!);
+
+  stdout.write('C: ');
+  int c = int.parse(stdin.readLineSync()!);
+
+  if ((a < (b + c)) && (b < (a + c)) && (c < (a + b))) {
+    if (a == b && b == c) {
+      print('EQUILATERAL.');
+    } else if (a == b || b == c || a == c) {
+      print('ISOSCELES.');
+    } else {
+      print('SCALENE.');
+    }
+  } else {
+    print('Not a triangle.');
+  }
+}
