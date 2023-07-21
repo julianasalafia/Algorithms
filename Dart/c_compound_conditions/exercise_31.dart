fcef6ecfe103e95761996910ac3bc5483ce81763
julianasalafia <juliana.salafia1@gmail.com>
2023-07-21 19:24:12 -0300

    [CC]: exercise 31

--- Dart/c_compound_conditions/exercise_31.dart
+++ Dart/c_compound_conditions/exercise_31.dart
@@ -4,43 +4,46 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  print('choose an option: \n1) rock \n2) paper \n3)scissors');
+  late String playerChoice;
+  late String computerChoice;
+
+  stdout.write('choose an option: \n1) rock \n2) paper \n3)scissors \n:: ');
   int playerOption = int.parse(stdin.readLineSync()!);
   int computerOption = Random().nextInt(3) + 1;
 
   if (playerOption == 1 && computerOption == 3) {
-    String playerChoice = 'rock';
-    String computerChoice = 'scissors';
+    playerChoice = 'rock';
+    computerChoice = 'scissors';
 
     print('Player: $playerChoice\nComputer: $computerChoice');
     print('Player wins.');
   } else if (playerOption == 1 && computerOption == 2) {
-    String playerChoice = 'rock';
-    String computerChoice = 'paper';
+    playerChoice = 'rock';
+    computerChoice = 'paper';
 
     print('Player: $playerChoice\nComputer: $computerChoice');
     print('Player looses.');
   } else if (playerOption == 2 && computerOption == 1) {
-    String playerChoice = 'paper';
-    String computerChoice = 'rock';
+    playerChoice = 'paper';
+    computerChoice = 'rock';
 
     print('Player: $playerChoice\nComputer: $computerChoice');
     print('Player wins.');
   } else if (playerOption == 2 && computerOption == 3) {
-    String playerChoice = 'paper';
-    String computerChoice = 'scissors';
+    playerChoice = 'paper';
+    computerChoice = 'scissors';
 
     print('Player: $playerChoice\nComputer: $computerChoice');
     print('Player looses.');
   } else if (playerOption == 3 && computerOption == 2) {
-    String playerChoice = 'scissors';
-    String computerChoice = 'paper';
+    playerChoice = 'scissors';
+    computerChoice = 'paper';
 
     print('Player: $playerChoice\nComputer: $computerChoice');
     print('Player wins.');
   } else if (playerOption == 3 && computerOption == 1) {
-    String playerChoice = 'scissors';
-    String computerChoice = 'rock';
+    playerChoice = 'scissors';
+    computerChoice = 'rock';
 
     print('Player: $playerChoice\nComputer: $computerChoice');
     print('Player loses.');
