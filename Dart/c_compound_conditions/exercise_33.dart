680c5e71a767103c9f6e4912904eea1b9e8ed6aa
julianasalafia <juliana.salafia1@gmail.com>
2023-07-22 20:51:07 -0300

    [CC]: exercise 33

--- Dart/c_compound_conditions/exercise_33.dart
+++ Dart/c_compound_conditions/exercise_33.dart
@@ -1,6 +1,8 @@
-// Write a program to approve or deny a bank loan to buy a house. The program will ask for the house value, the buyer's
-// salary, and in how many years he/she will pay. Calculate the monthly payment value, knowing that it cannot exceed
-// 30% of the salary, otherwise the loan will be denied.
+/*
+  Write a program to approve or deny a bank loan to buy a house. The program will ask for the house value, the buyer's
+  salary, and in how many years he/she will pay. Calculate the monthly payment value, knowing that it cannot exceed
+  30% of the salary, otherwise the loan will be denied.
+*/
 
 import 'dart:io';
 
