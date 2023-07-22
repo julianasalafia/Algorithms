cb2e5ef3c40f779cfbe7a875bbb5d1fdf09b9361
julianasalafia <juliana.salafia1@gmail.com>
2023-07-22 10:33:15 -0300

    [CC]: exercise 32

--- Dart/c_compound_conditions/exercise_32.dart
+++ Dart/c_compound_conditions/exercise_32.dart
@@ -1,1 +1,19 @@
 // [CHALLENGE] Create a game where the computer will draw a number between 1 and 5, and the player will try to guess the drawn value.
+
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  int computerDraw = Random().nextInt(5) + 1;
+
+  stdout.write('Type a number between 1 and 5: ');
+  int playerChoice = int.parse(stdin.readLineSync()!);
+
+  if (playerChoice != computerDraw) {
+    print('Number: $playerChoice \nDrawn value: $computerDraw.');
+  }
+
+  if (playerChoice == computerDraw) {
+    print('Number: $playerChoice \nDrawn value: $computerDraw.');
+  }
+}
