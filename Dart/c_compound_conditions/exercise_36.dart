039910df435d8a482f4703af43088096797b6bf1
julianasalafia <juliana.salafia1@gmail.com>
2023-07-24 20:01:48 -0300

    [CC]: exercise 36

--- Dart/c_compound_conditions/exercise_36.dart
+++ Dart/c_compound_conditions/exercise_36.dart
@@ -5,3 +5,60 @@
     For every point earned, the customer earns R$0.05 (5 cents) Write a program that reads how many hours of physical activity a person had
     in a month, calculates and shows how many points they earned and how much money they were able to earn.
 */
+
+import 'dart:io';
+
+enum ActivityStatus { LIGHT, MODERATE, HEAVY }
+
+void main() {
+  stdout.write('How many hours of physical activity did you had in a month? ');
+  double hoursOfActivity = double.parse(stdin.readLineSync()!);
+
+  ActivityStatus? activityStatus = setActivityStatus(hoursOfActivity);
+  double bonus = calculateBonus(activityStatus!, hoursOfActivity);
+  message(activityStatus, hoursOfActivity, bonus);
+}
+
+double calculateBonus(ActivityStatus category, double hoursOfActivity) {
+  double points;
+  double bonus = 0.05;
+
+  switch (category) {
+    case ActivityStatus.LIGHT:
+      points = 2;
+      break;
+    case ActivityStatus.MODERATE:
+      points = 5;
+      break;
+    case ActivityStatus.HEAVY:
+      points = 10;
+      break;
+  }
+  points = hoursOfActivity * points;
+  bonus = points * 0.05;
+
+  return bonus;
+}
+
+ActivityStatus? setActivityStatus(double hoursOfActivity) {
+  ActivityStatus? activityStatus;
+
+  if (hoursOfActivity >= 0 && hoursOfActivity < 10) {
+    return activityStatus = ActivityStatus.LIGHT;
+  }
+
+  if (hoursOfActivity >= 10 && hoursOfActivity < 20) {
+    return activityStatus = ActivityStatus.MODERATE;
+  }
+
+  if (hoursOfActivity >= 20) {
+    return activityStatus = ActivityStatus.HEAVY;
+  }
+  return activityStatus;
+}
+
+void message(
+    ActivityStatus? activityStatus, double hoursOfActivity, double bonus) {
+  print(
+      '${activityStatus.toString().split('.').last}: You did $hoursOfActivity hours of physical activity and earned US\$$bonus.');
+}
