ec6773f735d3211c1c8293badc49e99f3e8d40f1
julianasalafia <juliana.salafia1@gmail.com>
2023-07-26 13:23:03 -0300

    [WL]: exercise 38

--- Dart/d_while_loops/exercise_38.dart
+++ Dart/d_while_loops/exercise_38.dart
@@ -1,1 +1,13 @@
 // 38) Write a program that displays the following count on the screen: 6 7 8 9 10 11 Finished!
+
+import 'dart:io';
+
+void main() {
+  int counter = 6;
+
+  while (counter < 12) {
+    stdout.write('$counter, ');
+    counter++;
+  }
+  print('finished!');
+}
