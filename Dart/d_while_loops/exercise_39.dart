0b9513535c4d26fb32e146570a40d0deda94eab0
julianasalafia <juliana.salafia1@gmail.com>
2023-07-26 16:22:29 -0300

    [WL]: exercise 39

--- Dart/d_while_loops/exercise_39.dart
+++ Dart/d_while_loops/exercise_39.dart
@@ -1,1 +1,24 @@
-// 39) Make an algorithm that displays the following count on the screen: 10 9 8 7 6 5 4 3 Finished!
+// 39) Make an algorithm that displays the following count on the screen:
+// 10 9 8 7 6 5 4 3 Finished!
+
+import 'dart:io';
+
+void main() {
+  List<int> vector = [];
+  int counter = 10;
+  int minSize = 3;
+
+  while (counter >= minSize) {
+    vector.add(counter);
+    counter--;
+  }
+
+  for (int element in vector) {
+    if (element > minSize) {
+      stdout.write('$element, ');
+    } else {
+      stdout.write('$element. ');
+    }
+  }
+  print('finished!');
+}
