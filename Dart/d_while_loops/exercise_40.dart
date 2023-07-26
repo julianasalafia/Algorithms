207fb654a9c24985054ed13c14b15f1cd6e2c84d
julianasalafia <juliana.salafia1@gmail.com>
2023-07-26 16:33:09 -0300

    [WL]: exercise 40

--- Dart/d_while_loops/exercise_40.dart
+++ Dart/d_while_loops/exercise_40.dart
@@ -1,1 +1,25 @@
-// 40) Create an application that displays the following count on the screen: 0 3 6 9 12 15 18 Finished!
+// 40) Create an application that displays the following count on the screen:
+// 0 3 6 9 12 15 18 Finished!
+
+import 'dart:io';
+
+void main() {
+  List<int> vector = [];
+  int counter = 0;
+  int maxSize = 18;
+  int change = 3;
+
+  while (counter <= maxSize) {
+    vector.add(counter);
+    counter += change;
+  }
+
+  for (int element in vector) {
+    if (element < maxSize) {
+      stdout.write('$element, ');
+    } else {
+      stdout.write('$element. ');
+    }
+  }
+  print('finished!');
+}
