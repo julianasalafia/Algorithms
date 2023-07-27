d42214452cf4a8acded4691e9ee615cc552973e6
julianasalafia <juliana.salafia1@gmail.com>
2023-07-27 11:39:47 -0300

    [WL]: exercise 41

--- Dart/d_while_loops/exercise_41.dart
+++ Dart/d_while_loops/exercise_41.dart
@@ -1,1 +1,25 @@
-// 41) Develop a program that displays the following count on the screen: 100 95 90 85 80 … 0 Finished!
+// 41) Develop a program that displays the following count on the screen:
+// 100 95 90 85 80 … 0 Finished!
+
+import 'dart:io';
+
+void main() {
+  List<int> vector = [];
+  int counter = 100;
+  int minSize = 0;
+  int step = 5;
+
+  while (counter >= minSize) {
+    vector.add(counter);
+    counter -= step;
+  }
+
+  for (int element in vector) {
+    if (element > minSize) {
+      stdout.write('$element, ');
+    } else {
+      stdout.write('$element. ');
+    }
+  }
+  print('finished!');
+}
