3c49d9510ff8c4818e15dbc29a30330d28365c6d
julianasalafia <juliana.salafia1@gmail.com>
2023-07-28 18:21:21 -0300

    [WL]: exercise 45

--- Dart/d_while_loops/exercise_45.dart
+++ Dart/d_while_loops/exercise_45.dart
@@ -1,2 +1,51 @@
 // 45) The above program will have a problem when we enter the first value greater than the last.
 // Solve this problem with code that works in any situation.
+
+import 'dart:io';
+
+void main() {
+  List<int> vector = [];
+  int firstValue = readInt('first value: ');
+  int lastValue = readInt('last value: ');
+  int increment = readInt('increment: ');
+
+  int counter = firstValue;
+
+  if (firstValue > lastValue) {
+    while (counter >= lastValue) {
+      vector.add(counter);
+      counter -= increment;
+    }
+  } else if (lastValue > firstValue) {
+    while (counter <= lastValue) {
+      vector.add(counter);
+      counter += increment;
+    }
+  }
+
+  for (int element in vector) {
+    if (element != vector.last) {
+      stdout.write('$element, ');
+    } else {
+      stdout.write('$element. ');
+    }
+  }
+
+  print('finished!');
+}
+
+int readInt(String prompt) {
+  int? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = int.tryParse(input!);
+
+    if (value == null) {
+      print('Error: Please enter a valid number');
+    }
+  } while (value == null);
+
+  return value;
+}
