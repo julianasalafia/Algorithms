18f7439ebc67b9e703409ec31d0de8261382ce43
julianasalafia <juliana.salafia1@gmail.com>
2023-07-28 18:46:56 -0300

    [WL]: exercise 46

--- Dart/d_while_loops/exercise_46.dart
+++ Dart/d_while_loops/exercise_46.dart
@@ -1,1 +1,21 @@
 // 46) Create a program that calculates and displays the result of the sum between 6 + 8 + 10 + 12 + 14 + … + 98 + 100.
+
+void main() {
+  int startNumber = 6;
+
+  int sum = calculateSum(startNumber);
+  print('Sum: $sum');
+}
+
+int calculateSum(int number) {
+  int sum = 0;
+  int maxNumber = 100;
+  int increment = 2;
+
+  while (number <= maxNumber) {
+    sum += number;
+    number += increment;
+  }
+
+  return sum;
+}
