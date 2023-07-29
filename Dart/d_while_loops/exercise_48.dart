8c5db634fad4bfefd57a4210bad49297f3ea47e1
julianasalafia <juliana.salafia1@gmail.com>
2023-07-29 17:35:16 -0300

    [WL]: exercise 48

--- Dart/d_while_loops/exercise_48.dart
+++ Dart/d_while_loops/exercise_48.dart
@@ -5,15 +5,24 @@ import 'dart:io';
 const int maxSize = 7;
 
 void main() {
+  List<int> vector = [];
   int counter = 1;
   int sum = 0;
 
   while (counter <= maxSize) {
     stdout.write('type a number: ');
     int number = int.parse(stdin.readLineSync()!);
-
+    vector.add(number);
     sum += number;
     counter++;
   }
-  print('sum: $sum');
+
+  for (int i = 0; i < vector.length; i++) {
+    if (i != vector.length - 1) {
+      stdout.write('${vector[i]} + ');
+    } else {
+      stdout.write('${vector[i]} = ');
+    }
+  }
+  print('$sum');
 }
