053a7e7e9c5d4594b9d5f8e87e2fd62f329c49a7
julianasalafia <juliana.salafia1@gmail.com>
2023-07-29 16:35:10 -0300

    [WL]: exercise 47

--- Dart/d_while_loops/exercise_47.dart
+++ Dart/d_while_loops/exercise_47.dart
@@ -1,1 +1,21 @@
 // 47) Develop an application that displays the result of the expression 500 + 450 + 400 + 350 + 300 + … + 50 + 0 on the screen.
+
+void main() {
+  int startNumber = 500;
+  int sum = calculateSum(startNumber);
+  print('Sum: $sum.');
+}
+
+int calculateSum(int number) {
+  int sum = 0;
+
+  int minNumber = 0;
+  int decrement = 50;
+
+  while (number >= minNumber) {
+    sum += number;
+    number -= decrement;
+  }
+
+  return sum;
+}
