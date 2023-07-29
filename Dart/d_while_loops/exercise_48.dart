2b531383d4a47fb7ebec54976490c120f9ff1d55
julianasalafia <juliana.salafia1@gmail.com>
2023-07-29 17:00:07 -0300

    [WL]: exercise 48

--- Dart/d_while_loops/exercise_48.dart
+++ Dart/d_while_loops/exercise_48.dart
@@ -1,1 +1,19 @@
 // 48) Make a program that reads 7 integers and displays their sum at the end.
+
+import 'dart:io';
+
+const int maxSize = 7;
+
+void main() {
+  int counter = 1;
+  int sum = 0;
+
+  while (counter <= maxSize) {
+    stdout.write('type a number: ');
+    int number = int.parse(stdin.readLineSync()!);
+
+    sum += number;
+    counter++;
+  }
+  print('sum: $sum');
+}
