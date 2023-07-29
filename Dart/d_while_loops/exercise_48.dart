f7f20376ea2d6acfb7dba6332cbd70a24c7ec685
julianasalafia <juliana.salafia1@gmail.com>
2023-07-29 17:45:18 -0300

    [WL]: exercise 48

--- Dart/d_while_loops/exercise_48.dart
+++ Dart/d_while_loops/exercise_48.dart
@@ -5,18 +5,40 @@ import 'dart:io';
 const int maxSize = 7;
 
 void main() {
+  String answer = 'yes';
+
+  while (answer == 'yes') {
+    List<int> vector = readNumbers(maxSize);
+    int sum = calculateSum(vector);
+    printSum(vector, sum);
+
+    stdout.write('\ndo you want to try again? ');
+    answer = stdin.readLineSync()!.toLowerCase();
+  }
+}
+
+List<int> readNumbers(int maxSize) {
   List<int> vector = [];
   int counter = 1;
-  int sum = 0;
 
   while (counter <= maxSize) {
-    stdout.write('type a number: ');
-    int number = int.parse(stdin.readLineSync()!);
+    int number = readInt('type a number: ');
     vector.add(number);
-    sum += number;
     counter++;
   }
+  return vector;
+}
 
+int calculateSum(List<int> vector) {
+  int sum = 0;
+
+  for (int number in vector) {
+    sum += number;
+  }
+  return sum;
+}
+
+void printSum(List<int> vector, int sum) {
   for (int i = 0; i < vector.length; i++) {
     if (i != vector.length - 1) {
       stdout.write('${vector[i]} + ');
@@ -25,4 +47,20 @@ void main() {
     }
   }
   print('$sum');
+}
+
+int readInt(String prompt) {
+  int? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = int.tryParse(input!);
+
+    if (value == null || value < 0) {
+      print('Error: Please enter a valid number.');
+    }
+  } while (value == null || value < 0);
+
+  return value;
 }
