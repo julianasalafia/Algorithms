e09f7d2d568b56903a6109c75be7906bd52834b8
julianasalafia <juliana.salafia1@gmail.com>
2023-07-30 12:49:55 -0300

    [WL]: exercise 49

--- Dart/d_while_loops/exercise_49.dart
+++ Dart/d_while_loops/exercise_49.dart
@@ -1,1 +1,54 @@
 // 49) Create a program that reads 6 integers and displays how many of them are even and how many are odd at the end.
+
+import 'dart:io';
+
+const int maxSize = 6;
+
+void main() {
+  List<int> vector = readNumbers(maxSize);
+  oddOrEven(vector);
+}
+
+void oddOrEven(List<int> vector) {
+  int odd = 0;
+  int even = 0;
+
+  for (int number in vector) {
+    if (number % 2 == 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  }
+
+  print('There are $even even numbers and $odd odd numbers.');
+}
+
+List<int> readNumbers(int maxSize) {
+  List<int> vector = [];
+
+  int counter = 0;
+
+  while (counter < maxSize) {
+    int number = readInt('type a number: ');
+    vector.add(number);
+    counter++;
+  }
+  return vector;
+}
+
+int readInt(String prompt) {
+  int? value;
+
+  do {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    value = int.tryParse(input!);
+
+    if (value == null) {
+      print('Error: Please enter a valid number.');
+    }
+  } while (value == null);
+
+  return value;
+}
