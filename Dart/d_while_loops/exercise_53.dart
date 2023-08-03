44b3f58945311c9b9f626e6da53261b6a260e496
julianasalafia <juliana.salafia1@gmail.com>
2023-08-03 14:17:42 -0300

    [WL]: exercise 53

--- Dart/d_while_loops/exercise_53.dart
+++ Dart/d_while_loops/exercise_53.dart
@@ -3,3 +3,43 @@
     a) How many men were registered b) How many women were registered
     c) The average age of the group d) The average age of men e) How many women are over 20 years old
 */
+
+import 'dart:io';
+
+void main() {
+  int men = 0;
+  int women = 0;
+  int sumAgeGroup = 0;
+  int sumAgeMen = 0;
+  int womenOverTwenty = 0;
+  late int averageAgeGroup;
+  late int averageAgeMen;
+
+  for (int i = 1; i <= 5; i++) {
+    print('============= PERSON $i =============');
+    stdout.write('what\'s your age? ');
+    int age = int.parse(stdin.readLineSync()!);
+    stdout.write('what\'s your gender? \n1) male 2) female 3) other \n:: ');
+    int gender = int.parse(stdin.readLineSync()!);
+
+    if (gender == 1) {
+      men++;
+      sumAgeMen += age;
+    }
+
+    if (gender == 2) {
+      women++;
+
+      if (age >= 20) {
+        womenOverTwenty++;
+      }
+    }
+  }
+
+  sumAgeGroup += age;
+  averageAgeGroup = sumAgeGroup ~/ 5;
+  averageAgeMen = sumAgeMen ~/ men;
+
+  print(
+      'men registered: $men \nwomen registered: $women \naverage age: $averageAgeGroup \naverage age men: $averageAgeMen \nwomenOverTwenty: $womenOverTwenty');
+}
