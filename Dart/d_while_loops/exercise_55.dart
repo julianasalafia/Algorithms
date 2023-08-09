47b1f50c6866d2d631c64e59aa6ddf112931d295
julianasalafia <juliana.salafia1@gmail.com>
2023-08-09 15:13:41 -0300

    [WL]: exercise 55

--- Dart/d_while_loops/exercise_55.dart
+++ Dart/d_while_loops/exercise_55.dart
@@ -1,3 +1,8 @@
+/*
+    55) [CHALLENGE] Let’s improve the game we made in exercise 32. From now on,
+    the computer will randomly choose a number between 1 and 10 and the player will have 4 attempts to try to guess it.
+*/
+
 import 'dart:io';
 import 'dart:math';
 
