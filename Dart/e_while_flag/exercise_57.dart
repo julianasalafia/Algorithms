0b87da89f1fc07c7440c7829d0378d5aa2869eab
julianasalafia <juliana.salafia1@gmail.com>
2023-08-11 11:33:05 -0300

    [WF]: exercise 57

--- Dart/e_while_flag/exercise_57.dart
+++ Dart/e_while_flag/exercise_57.dart
@@ -3,3 +3,38 @@
     show the total amount paid to men and the total paid to women. The program will ask the user whether
     he wants to continue or not whenever he reads the data of an employee.
 */
+
+import 'dart:io';
+
+void main() {
+  double sumMenSalary = 0.0;
+  double sumWomenSalary = 0.0;
+
+  stdout.write('Do you want to register [Y/N]? ');
+  String answer = stdin.readLineSync()!.toLowerCase();
+
+  while (answer == 'y') {
+    stdout.write('salary: ');
+    double salary = double.parse(stdin.readLineSync()!);
+
+    stdout.write('sex [M/F]: ');
+    String gender = stdin.readLineSync()!.toLowerCase();
+
+    if (gender == 'm') {
+      sumMenSalary += salary;
+    }
+
+    if (gender == 'f') {
+      sumWomenSalary += salary;
+    }
+
+    stdout.write('do you want to register [y/n]: ');
+    answer = stdin.readLineSync()!.toLowerCase();
+
+    if (answer == 'n') {
+      stdout.write('the sum of men\'s salaries is $sumMenSalary. \n');
+      stdout.write('the sum of women\'s salaries is $sumWomenSalary.');
+      return;
+    }
+  }
+}
