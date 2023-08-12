f6939d4e2b1723bdedfeb514017ff4415a1dfcfd
julianasalafia <juliana.salafia1@gmail.com>
2023-08-12 00:49:03 -0300

    [WF]: exercise 58

--- Dart/e_while_flag/exercise_58.dart
+++ Dart/e_while_flag/exercise_58.dart
@@ -3,3 +3,33 @@
     The program will stop when the age 999 is entered. At the end, show how many students are in the
     class and what is the average age of the group.
 */
+
+import 'dart:io';
+
+void main() {
+  int count = 0;
+  List<int> ages = [];
+  int sumAges = 0;
+
+  stdout.write('Do you want to register students? [Y/N]? ');
+  String answer = stdin.readLineSync()!.toLowerCase();
+
+  while (answer == 'y') {
+    stdout.write('Age: ');
+    int age = int.parse(stdin.readLineSync()!);
+
+    if (age == 999) {
+      break;
+    }
+
+    sumAges += age;
+    ages.add(age);
+    count++;
+
+    stdout.write('Do you want to register students? [Y/N]? ');
+    answer = stdin.readLineSync()!.toLowerCase();
+  }
+
+  int averageAge = sumAges ~/ ages.length;
+  stdout.write('Number of students: $count \nAverage age: $averageAge.');
+}
