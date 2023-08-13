d9c48ce3015a7869b8e8b7d065c0340492e1e06e
julianasalafia <juliana.salafia1@gmail.com>
2023-08-13 12:19:39 -0300

    [WL]: exercise 58

--- Dart/e_while_flag/exercise_58.dart
+++ Dart/e_while_flag/exercise_58.dart
@@ -18,26 +18,40 @@ String getUserInput(String prompt) {
 
 List<int> registerStudents() {
   List<int> ages = [];
-  String answer = getUserInput('Do you want to register students? [Y/N]?');
+  String answer = getUserInput('Do you want to register students? [Y/N]? ');
 
   while (answer == 'y') {
     stdout.write('Age: ');
-    int age = int.parse(stdin.readLineSync()!);
+    String? input = stdin.readLineSync();
+
+    int age;
+
+    try {
+      age = int.parse(input!);
+    } catch (e) {
+      print('Invalid input. Please enter a valid age. ');
+      continue;
+    }
 
     if (age == 999) {
       break;
     }
 
     ages.add(age);
-    answer = getUserInput('Do you want to register students? [Y/N]?');
+    answer = getUserInput('Do you want to register students? [Y/N]? ');
   }
 
   return ages;
 }
 
 void displayResults(List<int> ages) {
+  if (ages.isEmpty) {
+    stdout.write('No students registered. ');
+    return;
+  }
+
   int sumAges = ages.fold(0, (prev, age) => prev + age);
   int averageAge = sumAges ~/ ages.length;
   stdout
-      .write('Number of students: ${ages.length} \nAverage age: $averageAge.');
+      .write('Number of students: ${ages.length} \nAverage age: $averageAge. ');
 }
