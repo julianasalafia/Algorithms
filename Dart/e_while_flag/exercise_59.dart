a2c1ae76c06d2750170af2af2a2c6739a40c9d68
julianasalafia <juliana.salafia1@gmail.com>
2023-08-15 10:21:15 -0300

    [WF]: exercise 59 - incomplete

--- Dart/e_while_flag/exercise_59.dart
+++ Dart/e_while_flag/exercise_59.dart
@@ -4,3 +4,33 @@
     a) what is the highest age read b) how many men were registered
     c) what is the age of the youngest woman d) what is the average age among men
 */
+
+import 'dart:io';
+
+void main() {
+  List<int> ages = registerUsers();
+  print('ages: $ages');
+}
+
+String getUserInput(String prompt) {
+  stdout.write(prompt);
+  return stdin.readLineSync()!.toLowerCase();
+}
+
+List<int> registerUsers() {
+  List<int> ages = [];
+  String answer = 'y';
+
+  while (answer == 'y') {
+    stdout.write('age: ');
+    String? input = stdin.readLineSync();
+
+    int age;
+
+    age = int.parse(input!);
+    ages.add(age);
+    answer = getUserInput('Do you want to register someone? [Y/N]? ');
+  }
+
+  return ages;
+}
