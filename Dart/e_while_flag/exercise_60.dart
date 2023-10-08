f89f359c6f4ff6ccc93ef343dd28f9af238d3889
julianasalafia <juliana.salafia1@gmail.com>
2023-10-08 12:28:52 -0300

    [WF]: exercise 60 - incomplete

--- Dart/e_while_flag/exercise_60.dart
+++ Dart/e_while_flag/exercise_60.dart
@@ -5,3 +5,34 @@
     c) the average age of the group d) how many men are over 30 years old
     e) how many women are under 18 years old
 */
+
+import 'dart:io';
+
+void main() {
+  String name = getUserInput('name: ');
+  int age = validateAgeInput();
+  String gender = validateGenderInput();
+}
+
+String getUserInput(String prompt) {
+  stdout.write(prompt);
+  return stdin.readLineSync()!.toLowerCase();
+}
+
+int validateAgeInput() {
+  int? age;
+  do {
+    stdout.write('age: ');
+    age = int.tryParse(stdin.readLineSync()!);
+  } while (age == null || age < 0);
+  return age;
+}
+
+String validateGenderInput() {
+  String gender;
+  do {
+    stdout.write('gender [M/F]: ');
+    gender = stdin.readLineSync()!.toUpperCase();
+  } while (gender != 'M' && gender != 'F');
+  return gender;
+}
