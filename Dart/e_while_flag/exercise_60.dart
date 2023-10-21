1f0cfa3e13e91f4cb0d7d52b199c85ad8710f297
julianasalafia <juliana.salafia1@gmail.com>
2023-10-20 23:43:47 -0300

    [WF]: exercise 60

--- Dart/e_while_flag/exercise_60.dart
+++ Dart/e_while_flag/exercise_60.dart
@@ -9,9 +9,32 @@
 import 'dart:io';
 
 void main() {
-  String name = getUserInput('name: ');
-  int age = validateAgeInput();
-  String gender = validateGenderInput();
+  final getInfo = signUpPeople();
+  message(getInfo);
+}
+
+List<People> signUpPeople() {
+  List<People> people = [];
+  String answer = 'Y';
+
+  do {
+    String name = getUserInput('name: ');
+    int age = validateAgeInput();
+    String gender = validateGenderInput();
+
+    people.add(
+      People(
+        name: name,
+        age: age,
+        gender: gender,
+      ),
+    );
+
+    answer = getUserInput('Do you want to register a new person? [Y/N]: ')
+        .toUpperCase();
+  } while (answer == 'Y');
+
+  return people;
 }
 
 String getUserInput(String prompt) {
@@ -35,4 +58,51 @@ String validateGenderInput() {
     gender = stdin.readLineSync()!.toUpperCase();
   } while (gender != 'M' && gender != 'F');
   return gender;
+}
+
+void message(List<People> people) {
+  int totalAge = 0;
+  int menOver30 = 0;
+  int womenUnder18 = 0;
+  People? oldestPerson;
+  People? youngestWoman;
+
+  for (var person in people) {
+    totalAge += person.age;
+
+    if (oldestPerson == null || person.age > oldestPerson.age) {
+      oldestPerson = person;
+    }
+
+    if (person.gender == 'F' &&
+        (youngestWoman == null || person.age < youngestWoman.age)) {
+      youngestWoman = person;
+    }
+
+    if (person.gender == 'M' && person.age > 30) {
+      menOver30++;
+    }
+
+    if (person.gender == 'F' && person.age < 18) {
+      womenUnder18++;
+    }
+  }
+
+  double averageAge = totalAge / people.length;
+
+  print('''
+  - The average age of the group is ${averageAge.toStringAsFixed(0)}
+      \n- The oldest person is called ${oldestPerson?.name}
+      \n- The youngest woman is called ${youngestWoman?.name}
+      \n- There are $menOver30 men over 30 years old
+      \n- There are $womenUnder18 women under 18 years old.
+      ''');
+}
+
+class People {
+  String name;
+  int age;
+  String gender;
+
+  People({required this.name, required this.age, required this.gender});
 }
